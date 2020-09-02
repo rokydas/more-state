5af2de39efbf4308ce59f94ec8f96e9a1c32d4eb
rokydas <droky136@gmail.com>
2020-09-03 00:06:32 +0600

    context api added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Shipment from './Components/Shipment/Shipment';
 import Home from './Components/Home/Home';
+
+export const categoryContext = createContext(); 
 
 function App() {
 
   const [ count, setCount ] = useState(0);
 
   return (
-    <div>
+    <categoryContext.Provider value={count}>
       <h3>Count: {count} (In app.js)</h3>
       <Header count={count} setCount={setCount}></Header>
       <Home count={count} setCount={setCount}></Home>
       <Shipment></Shipment>
-    </div>
+    </categoryContext.Provider>
   );
 }
 
--- src/Components/CategoryDetails/CategoryDetails.js
+++ src/Components/CategoryDetails/CategoryDetails.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext, createContext } from 'react';
+import { categoryContext } from '../../App';
 
 const CategoryDetails = (props) => {
     const { count, setCount } = props;
+
+    const category = useContext(categoryContext);
     return (
         <div>
             <h3>I am Category Details: {count}</h3>
+            <h5>Selected Category: {category}</h5>
         </div>
     );
 };
